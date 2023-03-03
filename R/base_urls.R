14bcf889177aa8db791717f1ea62e0d0136eb575
fnavarro94 <felinavarro94@gmail.com>
2023-03-03 08:35:32 -0500

    Update base_urls.R

--- R/base_urls.R
+++ R/base_urls.R
@@ -3,6 +3,6 @@
 #' @description
 #' These are the base URLs for the `rgpt3` package. Do not change these!
 #' @export
-url.completions = "https://api.openai.com/v1/chat/completions"
+url.completions = "https://api.openai.com/v1/completions"
 url.embeddings = "https://api.openai.com/v1/embeddings"
 url.fine_tune = "https://api.openai.com/v1/fine-tunes"
